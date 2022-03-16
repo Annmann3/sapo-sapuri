66c28a10d8e1a6367d13b78b3029603b845068a3
Annmann3 <kuroihaya@gmail.com>
2022-03-16 18:04:33 +0900

    chor: 不要なファイル生成しないようにした

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,10 @@ module SapoSapuri
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.stylesheets false
+      g.assets false
+      g.helper false
+    end
   end
 end
