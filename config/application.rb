5fb216e560b102d4f8f04b72aec78e4d16ff5070
Annmann3 <kuroihaya@gmail.com>
2022-03-17 17:40:19 +0900

    add: I18n User #15

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,9 @@ module SapoSapuri
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    # I18n設定ファイル
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{yml,rb}')]
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
