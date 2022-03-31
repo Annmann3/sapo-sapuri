b6afb3de008629ff4d9bd3790a0dd8a3621b1580
Annmann3 <kuroihaya@gmail.com>
2022-03-31 16:42:01 +0900

    chor : vue-loaderの設定

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,11 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
+environment.loaders.prepend('vue', {
+  test: /\.vue$/,
+  use: [{
+    loader: 'vue-loader'
+  }]
+})
 module.exports = environment
