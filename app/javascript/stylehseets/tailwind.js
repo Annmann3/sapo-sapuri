05dcee6ba2cd9e22355e07d27a1e3b6de3ac2efb
Annmann3 <kuroihaya@gmail.com>
2022-07-06 22:36:49 +0900

    webpackerからshakapackerへ

---
+++ app/javascript/stylehseets/tailwind.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [
+        '../**/*.vue',
+        '../packs/**/*.js',
+      
+  ],
+    darkMode: false, // or 'media' or 'class'
+  theme: {
+        extend: {},
+      
+  },
+  variants: {
+        extend: {},
+      
+  },
+  plugins: [
+        require('@tailwindcss/forms'),
+      
+  ],
+  
+}
+
