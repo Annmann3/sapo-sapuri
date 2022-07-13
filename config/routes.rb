10b7eaead7b42282cc0fc28d1a9e2f935aa21513
Annmann3 <kuroihaya@gmail.com>
2022-07-13 21:19:29 +0900

    Userのname属性を登録できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
       resources :nutrients, only: %i[index show]
       resources :graph, only: %i[show]
     end
