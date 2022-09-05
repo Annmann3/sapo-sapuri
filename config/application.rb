2c42d59944f25058e098b99b6cffde3209630a05
Annmann3 <kuroihaya@gmail.com>
2022-09-05 19:44:14 +0900

    タイムゾーンを変更

--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,9 @@ module SapoSapuri
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+    # タイムゾーン
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # I18n設定ファイル
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{yml,rb}')]
