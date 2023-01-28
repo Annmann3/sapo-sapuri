9b809e625cca4dd2406bdb95e3bd6dca95c8327d
Annmann3 <kuroihaya@gmail.com>
2023-01-28 14:13:36 +0900

    chor: gmail設定追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -37,7 +37,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'login',
+    enable_starttls_auto: true
+  }
   config.action_mailer.perform_deliveries = true
 
   # Action Mailerのホスト名
