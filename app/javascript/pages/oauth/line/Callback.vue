26df3267d2b7e562424b3280add76f2287615434
Annmann3 <kuroihaya@gmail.com>
2023-10-25 14:28:11 +0900

    fix: lineログイン後にグラフページにリダイレクトできない

--- app/javascript/pages/oauth/line/Callback.vue
+++ app/javascript/pages/oauth/line/Callback.vue
@@ -1,24 +1,32 @@
-<template>
-</template>
 
-<script>
-export default {
-  name: 'LineCallback',
-  async mounted() {
-    const { auth_token, client_id, uid, expiry, change_provider } = this.$route.query
-    debugger
-    if (change_provider === 'true') {
-      await this.$store.dispatch('users/updateProvider', { provider: 'line', uid: uid })
-    } else {
-      const headers = {
-        'access-token': auth_token,
-        'client': client_id,
-        'uid': uid,
-        'expiry': expiry,
-      }
-      await this.$store.dispatch('users/oauthSignin', headers)
-      this.$router.push('/')
-    }
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+
+const loading = ref(false)
+const error = ref(null)
+const route = useRoute()
+const router = useRouter()
+const store = useStore()
+
+onMounted(async() => {
+  debugger
+  const {auth_token, client_id, uid } = route.query
+  const headers = {
+    'access-token': auth_token,
+    'client': client_id,
+    'uid': uid,
   }
-}
+  try {
+    loading.value = true
+    await store.dispatch('users/oauthSignin', headers)
+    loading.value = false
+    router.push({ path: '/graph' })
+  } catch(err) {
+    error.value = err.response
+    store.commit('flashMessage/setFlashMessage', err.response)
+    router.push({ path: '/' })
+  }
+})
 </script>
