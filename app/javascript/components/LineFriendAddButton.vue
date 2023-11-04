16411a8dbf40b6f1c7262dcd6c93fff879d4c2f6
Annmann3 <kuroihaya@gmail.com>
2023-11-04 18:32:16 +0900

    add: line友だち追加ボタン

---
+++ app/javascript/components/LineFriendAddButton.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="line-it-button" data-lang="ja" data-type="friend" data-env="REAL" data-lineId="@433onjos" style="display: none;"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+onMounted(() => {
+  const script = document.createElement('script')
+  script.src = 'https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js'
+  script.async = true
+  script.defer = true
+  document.body.appendChild(script)
+})
+</script>
