d507cedb8d9f9eadad89f876eda43324c1fd1e56
Annmann3 <kuroihaya@gmail.com>
2023-11-04 16:01:44 +0900

    delete: 不要なデバッカ

--- app/javascript/pages/oauth/line/Callback.vue
+++ app/javascript/pages/oauth/line/Callback.vue
@@ -1,17 +1,13 @@
-
 <script setup>
-import { ref, onMounted } from 'vue'
+import { onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useStore } from 'vuex'
 
-const loading = ref(false)
-const error = ref(null)
 const route = useRoute()
 const router = useRouter()
 const store = useStore()
 
 onMounted(async() => {
-  debugger
   const {auth_token, client_id, uid } = route.query
   const headers = {
     'access-token': auth_token,
@@ -19,12 +15,9 @@ onMounted(async() => {
     'uid': uid,
   }
   try {
-    loading.value = true
     await store.dispatch('users/oauthSignin', headers)
-    loading.value = false
     router.push({ path: '/graph' })
   } catch(err) {
-    error.value = err.response
     store.commit('flashMessage/setFlashMessage', err.response)
     router.push({ path: '/' })
   }
