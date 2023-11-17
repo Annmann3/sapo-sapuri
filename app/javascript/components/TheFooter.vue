cf70f15bb343c40c0a18a15d125601a5d6a1bc85
Annmann3 <kuroihaya@gmail.com>
2023-11-17 14:36:58 +0900

    問い合わせリンクを別タブ表示

--- app/javascript/components/TheFooter.vue
+++ app/javascript/components/TheFooter.vue
@@ -5,7 +5,7 @@
         <li class="mb-0 md:mb-2"><router-link to="/terms" class="text-gray-700 text-sm ">利用規約</router-link></li>
         <li><router-link to="/privacy-policy" class="text-gray-700 text-sm">プライバシポリシー</router-link></li>
         <li><router-link to="/notes" class="text-gray-700 text-sm">注意事項</router-link></li>
-        <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSecvyGVJm10hXeMhOgVCUgVqJIcT1b8z48fAbDnhUb-hC5-UA/viewform?usp=sf_link" class="text-gray-700 text-sm">お問い合わせ</a></li>
+        <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSecvyGVJm10hXeMhOgVCUgVqJIcT1b8z48fAbDnhUb-hC5-UA/viewform?usp=sf_link" class="text-gray-700 text-sm" target="_blank">お問い合わせ</a></li>
       </ul>
       <div class="mb-2 md:mb-0">
         <p class="text-sm font-semibold">&copy;  サポC 2023</p>
