f913c350e9a4138686269803b3f93d469cc6cd08
Annmann3 <kuroihaya@gmail.com>
2023-11-29 17:49:08 +0900

    api 期間内の服用記録がないときの処理

--- app/controllers/api/v1/user_graph_controller.rb
+++ app/controllers/api/v1/user_graph_controller.rb
@@ -1,12 +1,15 @@
 class Api::V1::UserGraphController < ApplicationController
   before_action :authenticate_api_v1_user!, only: %i[index]
 
-  # 24時間以内の服用したサプリのグラフデータ
   def index
-    dosages = current_api_v1_user.dosages.in_24hours
+    dosages = current_api_v1_user.dosages.in_week_asc
 
     if dosages.blank?
-      render json: Nutrient.find(1).calculate_zero
+      render json: {
+        graph_data: Nutrient.find(1).calculate_zero,
+        goal: Nutrient.find(1).calculate_goal(nil),
+        achievement_rate: Dosage.weekly_achievement_rate(current_api_v1_user)
+      }
     else
       render json: {
         graph_data: Nutrient.find(1).calculate_combined_24hours(dosages),
